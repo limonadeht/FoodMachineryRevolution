4fecd1115b5039ab304bbe2965959d237cf8d972
limonadeht <a89sserver1_7_2@yahoo.co.jp>
2015-12-29 23:16:36 +0900

    MaterialRegister Fix

--- src/common/MaterialRegister.java
+++ src/common/MaterialRegister.java
@@ -1,5 +1,8 @@
 package common;
-
+/*
+ * https://github.com/defeatedcrow/AppleMilkTea2_1.7.10/blob/master/java/mods/defeatedcrow/common/MaterialRegister.java#L942
+ * 参考までに、出来るだけスッキリ綺麗なコードに
+ */
 public class MaterialRegister {
 
 	public static MaterialRegister instance = new MaterialRegister();
