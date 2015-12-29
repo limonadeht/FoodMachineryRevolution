3ccf5bd7882077cef39c1b280f2cd6c39d6eec15
limonadeht <a89sserver1_7_2@yahoo.co.jp>
2015-12-29 23:14:35 +0900

    Registerクラス追加

---
+++ src/common/MaterialRegister.java
@@ -0,0 +1,33 @@
+package common;
+
+public class MaterialRegister {
+
+	public static MaterialRegister instance = new MaterialRegister();
+
+	private MaterialRegister(){
+	}
+
+	public void load(){
+	}
+
+	public void addFluid(){
+	}
+
+	static void addTools(){
+	}
+
+	static void addFoods(){
+	}
+
+	static void addBlocks(){
+	}
+
+	static void addPlants(){
+	}
+
+	static void addMaterials(){
+	}
+
+	static void addMachines(){
+	}
+}
